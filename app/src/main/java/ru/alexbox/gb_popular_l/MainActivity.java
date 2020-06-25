8dd3fcc6c69f8399d28ac0f99cc349501b66c1e2
Alex Podgurski <alexbox726@gmail.com>
2020-06-25 23:41:43 +0300

    Lesson_1

---
+++ app/src/main/java/ru/alexbox/gb_popular_l/MainActivity.java
@@ -0,0 +1,43 @@
+package ru.alexbox.gb_popular_l;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import ru.alexbox.gb_popular_l.MVP.MVPActivity;
+import ru.alexbox.gb_popular_l.MVP_Moxy.MoxyActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Buttons();
+    }
+
+    private void Buttons() {
+        Button ButtonMVP = findViewById(R.id.BtnMVP);
+        Button ButtonMoxy = findViewById(R.id.BtnMoxy);
+
+        ButtonMVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMV = new Intent(getApplicationContext(), MVPActivity.class);
+                startActivity(intentMV);
+            }
+        });
+
+        ButtonMoxy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMO = new Intent(getApplicationContext(), MoxyActivity.class);
+                startActivity(intentMO);
+            }
+        });
+    }
+}
