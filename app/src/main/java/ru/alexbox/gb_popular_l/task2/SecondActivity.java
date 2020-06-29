8c6f2c2738b8c46220a0566f6c0605db860c42cc
Alex Podgurski <alexbox726@gmail.com>
2020-06-29 21:34:00 +0300

    Lesson_2
    Shell is ready

---
+++ app/src/main/java/ru/alexbox/gb_popular_l/task2/SecondActivity.java
@@ -0,0 +1,16 @@
+package ru.alexbox.gb_popular_l.task2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.alexbox.gb_popular_l.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
