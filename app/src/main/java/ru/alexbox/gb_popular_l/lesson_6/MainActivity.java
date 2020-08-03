377dde32c3df1a40deb4811e98826136adf636bb
Alex Podgurski <alexbox726@gmail.com>
2020-08-03 23:29:29 +0300

    Lesson_7
    Clear project

--- app/src/main/java/ru/alexbox/gb_popular_l/lesson_6/MainActivity.java
+++ app/src/main/java/ru/alexbox/gb_popular_l/lesson_6/MainActivity.java
@@ -2,14 +2,9 @@ package ru.alexbox.gb_popular_l.lesson_6;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import ru.alexbox.gb_popular_l.R;
-import ru.alexbox.gb_popular_l.lesson_6.dagger.DaggerActivity;
-import ru.alexbox.gb_popular_l.lesson_6.di.DiActivity;
-import ru.alexbox.gb_popular_l.lesson_6.nodi.NoDiActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,28 +12,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initButtons();
-    }
-
-    private void initButtons() {
-        Button btnOne = findViewById(R.id.btnOne);
-        Button btnTwo = findViewById(R.id.btnTwo);
-        Button btnThree = findViewById(R.id.btnThree);
-
-        btnOne.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), NoDiActivity.class);
-            startActivity(intent);
-        });
-
-        btnTwo.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), DiActivity.class);
-            startActivity(intent);
-        });
-
-        btnThree.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplicationContext(), DaggerActivity.class);
-            startActivity(intent);
-        });
     }
 }
