f64f737b3a1b474a5903a373c68f10d4722bb395
Alex Podgurski <alexbox726@gmail.com>
2020-08-04 21:46:00 +0300

    Lesson_7
    Final

--- app/src/main/java/ru/alexbox/gb_popular_l/lesson_7/unit/task_1.java
+++ app/src/main/java/ru/alexbox/gb_popular_l/lesson_7/unit/task_1.java
@@ -3,6 +3,6 @@ package ru.alexbox.gb_popular_l.lesson_7.unit;
 public class task_1 {
 
     public int plusArgs(int arg1, int arg2) {
-        return arg1 += arg2;
+        return arg1 + arg2;
     }
 }
