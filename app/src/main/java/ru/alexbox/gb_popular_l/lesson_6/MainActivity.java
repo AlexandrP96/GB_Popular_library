d2b544bca86addd38ce4fbf4b690146674646b39
Alex Podgurski <alexbox726@gmail.com>
2020-08-04 19:50:26 +0300

    Lesson_7
    Task Two Shell is Done

--- app/src/main/java/ru/alexbox/gb_popular_l/lesson_6/MainActivity.java
+++ app/src/main/java/ru/alexbox/gb_popular_l/lesson_6/MainActivity.java
@@ -3,14 +3,31 @@ package ru.alexbox.gb_popular_l.lesson_6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import ru.alexbox.gb_popular_l.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.taskTwoView);
+        button = findViewById(R.id.btnTaskTwo);
+        textView.setVisibility(View.INVISIBLE);
+
+        buttonLogic();
     }
+
+    private void buttonLogic() {
+        button.setOnClickListener(v -> textView.setVisibility(View.VISIBLE));
+    }
+
 }
